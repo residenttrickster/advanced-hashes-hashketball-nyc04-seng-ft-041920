23c6bedf9749c101803cd51b226a06cc0a63f42a
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-06 15:47:20 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,79 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player)
+  game_hash.each do |location, value|
+    value[:players].each do |key|
+    if player == key[:player_name]
+      return key[:points]
+    end
+   end
+  end
+end
+
+def shoe_size(player)
+  game_hash.each do |location, value|
+    value[:players].each do |key|
+    if player == key[:player_name]
+      return key[:shoe]
+    end
+   end
+  end
+end
+
+def team_colors(name)
+  game_hash.each do |location, value|
+    if name == value[:team_name]
+    return value[:colors]
+    end
+  end
+end
+
+def team_names
+  names_array = []
+  game_hash.each do |location, value|
+    names_array << value[:team_name]
+  end
+  return names_array
+end
+
+def player_numbers(team_name)
+  jersey_number = []
+  game_hash.each do |location, value|
+      if team_name == value[:team_name]
+        value[:players].each do |key|
+        jersey_number << key[:number]
+      end
+    end
+  end
+  return jersey_number
+end
+
+#Build a method, player_stats, that takes in an argument of a player's name and returns a hash of that player's stats.
+
+def player_stats(name)
+  stats = {}
+  game_hash.each do |location, value|
+    value[:players].each do |key|
+    if name == key[:player_name]
+      stats = key
+    end
+  end
+end
+  return stats
+end
+
+#Then, return that player's number of rebounds. Remember to think about return values here.
+def big_shoe_rebounds
+  shoe_size = 0
+  rebounds = 0
+  game_hash.each do |location, value|
+    value[:players].each do |key|
+      if shoe_size < key[:shoe]
+        shoe_size = key[:shoe]
+        rebounds = key[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
